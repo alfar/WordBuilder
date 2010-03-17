319335d3328ccdc3daf29f0896fbb81827b40cbc
Arne Sostack <github_arne@whee.dk>
2010-03-17 20:46:18 +0100

    Added DictionaryStatisticsDialog v1.0

---
+++ WordBuilder2/Forms/DictionaryStatisticsDialog.Designer.vb
@@ -0,0 +1,97 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DictionaryStatisticsDialog
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
+        Me.OK_Button = New System.Windows.Forms.Button
+        Me.StatisticsLabel = New System.Windows.Forms.Label
+        Me.CopyButton = New System.Windows.Forms.Button
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 73.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.CopyButton, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(275, 274)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(148, 29)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.Location = New System.Drawing.Point(4, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "OK"
+        '
+        'StatisticsLabel
+        '
+        Me.StatisticsLabel.Location = New System.Drawing.Point(12, 9)
+        Me.StatisticsLabel.Name = "StatisticsLabel"
+        Me.StatisticsLabel.Size = New System.Drawing.Size(411, 262)
+        Me.StatisticsLabel.TabIndex = 1
+        Me.StatisticsLabel.Text = "Label1"
+        '
+        'CopyButton
+        '
+        Me.CopyButton.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CopyButton.Location = New System.Drawing.Point(78, 3)
+        Me.CopyButton.Name = "CopyButton"
+        Me.CopyButton.Size = New System.Drawing.Size(67, 23)
+        Me.CopyButton.TabIndex = 1
+        Me.CopyButton.Text = "&Copy"
+        '
+        'DictionaryStatisticsDialog
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(435, 315)
+        Me.Controls.Add(Me.StatisticsLabel)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "DictionaryStatisticsDialog"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Dictionary Statistics"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents StatisticsLabel As System.Windows.Forms.Label
+    Friend WithEvents CopyButton As System.Windows.Forms.Button
+
+End Class
---
+++ WordBuilder2/Forms/DictionaryStatisticsDialog.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Windows.Forms
+
+Public Class DictionaryStatisticsDialog
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub DictionaryStatisticsDialog_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim sb As New System.Text.StringBuilder()
+
+        sb.AppendFormat("Total words: {0}{1}", DictionaryData.Word.CountTotal(), vbCrLf)
+        sb.AppendFormat("Root words: {0}{1}", DictionaryData.Word.CountRootWords(), vbCrLf)
+        sb.AppendFormat("Unlinked words: {0}{1}", DictionaryData.Word.CountUnlinkedWords(), vbCrLf)
+        sb.AppendFormat("Unlinked root words: {0}{1}", DictionaryData.Word.CountUnlinkedWords(), vbCrLf)
+        sb.AppendFormat("Total links: {0}{1}", DictionaryData.Relation.CountTotal(), vbCrLf)
+        sb.AppendFormat("Total marks: {0}{1}", DictionaryData.Mark.CountTotal(), vbCrLf)
+        sb.AppendFormat("Mark types in use: {0}{1}", DictionaryData.MarkType.CountTotal(), vbCrLf)
+
+        StatisticsLabel.Text = SB.ToString()
+    End Sub
+
+    Private Sub CopyButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CopyButton.Click
+        My.Computer.Clipboard.SetText(StatisticsLabel.Text)
+    End Sub
+End Class
