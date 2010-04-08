f957c4c2e014d53b34caafc6f26fd0aa86aa6d7b
Arne Sostack <github_arne@whee.dk>
2010-04-08 20:48:23 +0200

    Removed last vb file.

--- SolutionInfo.vb
+++
@@ -1,9 +0,0 @@
-Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
-
-<Assembly: AssemblyDescription("Thank you for using WordBuilder. Visit the forums at http://whee.dk/ and let me know what you think of it.")> 
-<Assembly: AssemblyCompany("Whee.dk")> 
-<Assembly: AssemblyCopyright("Copyright © Whee.dk 2010")> 
-<Assembly: AssemblyVersion("2.0.5.0")> 
-<Assembly: AssemblyFileVersion("2.0.5.0")> 
